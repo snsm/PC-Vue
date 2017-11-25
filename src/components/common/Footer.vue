5e357d02d01d4067a342fb466617faabbed18a44
陈小龙 <13480731740@qq.com>
2017-11-25 23:26:34 +0800

    头部底部完成

---
+++ src/components/common/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="footer">
+    <div class="app-foot">
+      <p>© 2017 义门陈超市</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/common/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header">
+    <div class="app-head">
+      <div class="app-head-inner">
+        <router-link :to="{path: '/'}"></router-link>
+        <div class="head-nav">
+          <ul class="nav-list">
+            <li>登录</li>
+            <li class="nav-pile">|</li>
+            <li>注册</li>
+            <li class="nav-pile">|</li>
+            <li>关于</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Herder',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
